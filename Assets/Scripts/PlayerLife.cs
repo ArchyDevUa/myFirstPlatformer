7defad76b09bfee827c736e31d7151e6e31e3dcc
Archy <archydevua@gmail.com>
2023-04-01 23:05:33 +0300

    added restart logic

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -2,21 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
+    private Rigidbody2D rb;
      private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Trap"))
         {
-            anim.SetTrigger("death");
+           Death();
         }
+    }
+
+    private void Death()
+    {
+        anim.SetTrigger("death");
+        rb.bodyType = RigidbodyType2D.Static;
+        
+    }
+
+    private void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
